4ef8758c6367ae1e330abdd5c334aac789088287
Nisarg Mahyavanshi <52291990+Nisarg851@users.noreply.github.com>
2022-03-08 21:46:46 +0530

    Update mapping.sol

--- mapping.sol
+++ mapping.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 contract Maps{
-    // Mapping 
-    mapping(string => int) internal investmentReturns;
+    // Mapping: It is used to map/pair two similar/different type of data together
+    // for ex: we can create a mapping to check for a person's income using their name
+    mapping(string => uint) private incomeOf;
 
-    function setInvestmentReturnsOf(string memory _name, int _value) public{
-        investmentReturns[_name] = _value;
+    function getIncomeOf(string memory _name) public view returns(uint){
+        return incomeOf[_name];
     }
-
-    function getInvestmentReturnsOf(string memory _name) public view returns(int){
-        return investmentReturns[_name];
+    
+    function setIncomeOf(string memory _name, uint income) public {
+        incomeOf[_name] = income;
     }
 }
