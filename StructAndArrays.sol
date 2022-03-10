f058a1485290dd38d68b16c191bf0009a8e6989c
Nisarg Mahyavanshi <52291990+Nisarg851@users.noreply.github.com>
2022-03-10 22:21:19 +0530

    Updated content in Array

--- StructAndArrays.sol
+++ StructAndArrays.sol
@@ -21,18 +21,57 @@ contract StructAndArrays{
                                                          // Yet bob and bob2 are both different entities
                                                          // evethough we have specified an id field to make a person unique, it is still a logical field and thus needs to be
                                                          // handled accordingly
-    // Arrays : A linear collection of a specific data-type
+    // Arrays : A linear collection of data of a specific data-type
     // arrays can be of primitive as well as user-defined data-type as shown below
     // fixed size arrays: Size of the array is specified in declaration
     int[5] public integers = [1,2,3,4,5];
-    // dynamic arrays: Size of the arrya can be changed by adding/removing elements
-    People[] internal group;
     
+    // dynamic arrays: Size of the arrya can be changed by adding/removing elements
+    People[] internal group;   
+    
+    // adding element at the end of array
     function addInGroup(uint _id, string memory _name) public{
         group.push(People({id:_id, name:_name}));
     }
-
-    function personInGroupWithID(uint id) public view returns(People memory){
-        return group[id];
+    
+    // accessing element at a specific index
+    function personInGroupWithID(uint _index) public view returns(People memory){
+        return group[_index];
     }
+    
+    // getting array's length
+    function getGroupLength() public view returns(uint){
+        return group.length;
+    }
+    
+    // removing the last element in array
+    function removeLastElement() public {
+        group.pop();
+    }
+    
+    // Removing an element from an array without changing array's length
+    function removeFromGroupArrayWithoutChangingTheLength(uint _index) public {
+        delete group[index];    // This line will not reduce the size of array
+                                // Instead it will just replace the removed items place with the default value
+                                // for ex: consider a uint array [1,2,3]
+                                // if 2 is removed from [1,2,3] then the array will become [1,0,3] , 0 is the default value for uint
+    }
+
+    // Method 1: Removing an element form an array and reducing array's size respectively
+    // Disadvantage: Gas costly
+    // Advantage: Maintains the elements of array in proper order
+    function removeElementMethodOne(uint _index) public {
+        for(uint start=index+1; start<group.length; group++){
+            group[start-1] = group[start];
+        }
+        group.pop();    // removes the last element of array and reduce the length of array by 1
+    }
+
+    // Method 2: Removing an element from an array and reducing array's size respectively
+    // Disadvantage: Alters the order of elements in array
+    // Advantage: Gas efficient
+    function removeElementMethodTwo(uint _index) public{
+        group[_index] = group[group.length-1];
+        group.pop();
+    }  
 }
