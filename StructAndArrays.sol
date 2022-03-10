b04601658e733426ac18d129a24bb7b9aafe4a59
Nisarg Mahyavanshi <52291990+Nisarg851@users.noreply.github.com>
2022-03-10 22:28:36 +0530

    Removed Experimental Erros

--- StructAndArrays.sol
+++ StructAndArrays.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
 
 contract StructAndArrays{
     // Struct : This are user defined data-type that are formed by combining more than one primitive type together.
@@ -27,15 +28,15 @@ contract StructAndArrays{
     int[5] public integers = [1,2,3,4,5];
     
     // dynamic arrays: Size of the arrya can be changed by adding/removing elements
-    People[] internal group;   
+    Person[] internal group;   
     
     // adding element at the end of array
     function addInGroup(uint _id, string memory _name) public{
-        group.push(People({id:_id, name:_name}));
+        group.push(Person({id:_id, name:_name}));
     }
     
     // accessing element at a specific index
-    function personInGroupWithID(uint _index) public view returns(People memory){
+    function personInGroupWithID(uint _index) public view returns(Person memory){
         return group[_index];
     }
     
@@ -51,7 +52,7 @@ contract StructAndArrays{
     
     // Removing an element from an array without changing array's length
     function removeFromGroupArrayWithoutChangingTheLength(uint _index) public {
-        delete group[index];    // This line will not reduce the size of array
+        delete group[_index];    // This line will not reduce the size of array
                                 // Instead it will just replace the removed items place with the default value
                                 // for ex: consider a uint array [1,2,3]
                                 // if 2 is removed from [1,2,3] then the array will become [1,0,3] , 0 is the default value for uint
@@ -61,7 +62,7 @@ contract StructAndArrays{
     // Disadvantage: Gas costly
     // Advantage: Maintains the elements of array in proper order
     function removeElementMethodOne(uint _index) public {
-        for(uint start=index+1; start<group.length; group++){
+        for(uint start=_index+1; start<group.length; start++){
             group[start-1] = group[start];
         }
         group.pop();    // removes the last element of array and reduce the length of array by 1
